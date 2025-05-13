7a1e1c35c4bf63ef45bc89f48750fc4a203aa014
Pradee Kumar <kumarpradeep895355@gmail.com>
2025-05-13 14:45:48 +0530

    fix icon issue

--- src/components/header/HeroSection.js
+++ src/components/header/HeroSection.js
@@ -6,16 +6,21 @@ const HeroSection = () => {
   return (
     <>
     <section className="d-flex align-items-center position-relative z-10">
-            <div className="container text-white">
+            <div className="container text-white mt-5">
               <h1 className="display-1s banner-img-heading-text">EXPERIENCE NORWAY <br /> IN MINIATURE</h1>
-              <p className="mt-3 telemark-sub-heading text-start">From the historic telemark canal to the beautiful coastline in just a few hours</p>
+              <p className="mt-3 telemark-sub-heading text-start">From the historic telemark canal to the beautiful <br /> coastline in just a few hours</p>
               <button className="btn  px-5 py-3 mt-4"> 
                 <img src={BookNow} alt='book now' className='img-fluid'/>
               </button>
               <p className="my-5 text-center mb-5">Find certified boats in Norway, available for bareboat or skippered charters.</p>
             </div>
           </section>
-        <CalenderInput />
+        {/* <CalenderInput /> */}
+
+          {/* Overlapping Calendar */}
+        <div className="calendar-wrapper container position-absolute start-50 translate-middle-x mb-5">
+          <CalenderInput />
+        </div>
         <div className="position-fixed bottom-0 end-0  mb-5" style={{ zIndex: 9999 }}>
         <button className="chat-icon">
           <img src={CallIcon}  alt="Call Icon" />
